7e80b685cff8d501233c240bbdecd88815cd9cbe
Petr Ungar <ungarpet@fel.cvut.cz>
2021-02-15 19:42:21 +0100

    rename tasks to solvers mods to templates

--- hiring/home/urls.py
+++ hiring/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html")),
-    path('login/', TemplateView.as_view(template_name="login.html")),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('sign_up/', TemplateView.as_view(template_name="sign_up.html"), name="signup"),
 ]
