1fbe6de9863ee5f31c57f70a09715530281c581f
Petr Ungar <ungarpet@fel.cvut.cz>
2021-02-17 21:52:09 +0100

    change ChallengeView to templateview add files

--- hiring/home/urls.py
+++ hiring/home/urls.py
@@ -1,11 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic import TemplateView
-from django.contrib.auth import views as auth_views
-from .views import ChallengeView
 
+from .views import ChallengeView
 
 urlpatterns = [
     path('', ChallengeView.as_view()),
+    path('log_cabin_challenge/', ChallengeView.as_view()),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('sign_up/', TemplateView.as_view(template_name="sign_up.html"), name="signup"),
 ]
--- hiring/home/views.py
+++ hiring/home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView
+
 from .models import Challenge
 
 
-class ChallengeView(ListView):
-    model = Challenge
+class ChallengeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['challenges'] = Challenge.objects.filter(is_published=True).order_by('created_date_time')[:6]
+        return context
+
+
+# CHALLENGES
+class LogCabinView(TemplateView):
+    template_name = 'home.html'
 
     def get(self, request, *args, **kwargs):
         challenges = Challenge.objects.filter(is_published=True).order_by('created_date_time')
-        return render(request, template_name='home.html', context={"challenges" : challenges})
+        return render(request, template_name='home.html', context={"challenges": challenges})
