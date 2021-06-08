c8d38cb6edd8c4339d3cee03a66b6a8dbf674b7e
Petr Ungar <ungarpet@fel.cvut.cz>
2021-06-08 17:13:05 +0200

    save

--- hiring/settings.py
+++ hiring/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-GCP = False
+GCP = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -38,7 +38,6 @@ LOGGING_LEVEL = logging.DEBUG
 LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_TO_CONSOLE = True
 
-#DataFlair
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
